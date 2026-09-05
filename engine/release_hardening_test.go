cf217ee3b202d105e2360bbc0101ca8eda869b8d
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-05 11:29:00 -0700

    test: gofmt release hardening snapshot regression

--- engine/release_hardening_test.go
+++ engine/release_hardening_test.go
@@ -1,16 +1,16 @@
 package engine
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 
-    "a2ui/protocol"
+	"a2ui/protocol"
 )
 
 func TestPresentationSnapshotCarriesRenderGeneration(t *testing.T) {
-    eng := New(protocol.DefaultLimits(), 16, nil)
-    snap := eng.PresentationSnapshot()
-    if _, ok := reflect.TypeOf(snap).FieldByName("RenderGeneration"); !ok {
-        t.Fatal("PresentationSnapshot must expose RenderGeneration for monotonic remote snapshots")
-    }
+	eng := New(protocol.DefaultLimits(), 16, nil)
+	snap := eng.PresentationSnapshot()
+	if _, ok := reflect.TypeOf(snap).FieldByName("RenderGeneration"); !ok {
+		t.Fatal("PresentationSnapshot must expose RenderGeneration for monotonic remote snapshots")
+	}
 }
