0a1a162f50b7081415bb09d9649c9e333d9cc481
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-07 18:58:22 -0700

    feat(daemon): handle semantic renderer interactions

--- daemon/interactions.go
+++ daemon/interactions.go
@@ -28,9 +28,15 @@ func (d *Daemon) handleInteraction(ctx context.Context, in *ipc.Interaction) *ip
 		perr = d.Engine.Submit(in.ID)
 	case ipc.InteractionTableMove:
 		perr = d.Engine.MoveTableSelection(in.ID, in.Delta)
+	case ipc.InteractionTableSelect:
+		perr = d.Engine.SetTableSelectionByRowID(in.ID, in.RowID)
 	case ipc.InteractionTableActivate:
 		perr = d.Engine.ActivateTableSelection(in.ID)
+	case ipc.InteractionActionInvoke:
+		perr = d.Engine.InvokeAction(ctx, in.ID, in.Action, in.Args)
 	case ipc.InteractionActionKey:
+		// Compatibility path for older local clients. New renderers translate
+		// physical input into semantic action invocation before crossing IPC.
 		perr = d.Engine.HandleKey(ctx, in.Key)
 	default:
 		return ipc.NewError("ipc.invalid_interaction", "unsupported interaction")
